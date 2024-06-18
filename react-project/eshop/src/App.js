055130ac964622124c6eadead61a319df1c9e299
anjalipatel93 <anjalipatel5691@gmail.com>
2024-06-18 11:14:33 +0200

    header of page

---
+++ react-project/eshop/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+   
+    <div className="App">
+      <div className="header_logo">
+        <div className="header_logoImage"></div>
+        <div className="header_shopName"></div>
+      </div>
+
+      <div className="header_search"></div>
+      <div className="header_bucket"></div>
+    </div>
+  );
+}
+
+export default App;
