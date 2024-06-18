fb01ef7a441960a946a0b7fc3944f0c95754fc21
anjalipatel93 <anjalipatel5691@gmail.com>
2024-06-18 17:53:26 +0200

    Header Bar

--- react-project/eshop/src/App.js
+++ react-project/eshop/src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import LoginIcon from '@mui/icons-material/Login';
+
 
 function App() {
   return (
    
     <div className="App">
       <div className="header_logo">
-        <div className="header_logoImage"></div>
-        <div className="header_shopName"></div>
+        <div className="header_logoImage">My Site Logo Should be Here</div>
+        <div className="header_shopName">My Shopping Site Name</div>
       </div>
-
-      <div className="header_search"></div>
-      <div className="header_bucket"></div>
+      <div className="header_searchBar">
+        <div className="header_searchBox">  <input type="text" name="inputTextBox" /></div>
+        <div className="header_searchBoxMagnifier"><SearchIcon /> </div>
+      </div>
+      <div className="header_search"><ShoppingBasketIcon /> </div>
+      <div className="header_bucket"><LoginIcon/></div>
     </div>
   );
 }
